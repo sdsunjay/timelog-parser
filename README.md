050d8f557b4be2ebd7451e54f36ea5df9782d61b
Sunjay <sdsunjay@users.noreply.github.com>
2023-08-22 06:44:08 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,90 @@
-# timelog-parser
-The timelog.py script provides functionality to parse, process, and reorder timelog data stored in HTML tables.
+# Time Log HTML Table Parser
+
+## Description
+
+The `timelog.py` script provides functionality to parse, process, and reorder timelog data stored in HTML tables. The primary purpose is to calculate the time difference (in minutes) between start and end times for a given activity and reorder the table entries accordingly.
+
+## Table Structure
+
+The script expects the table to have the following column headings:
+
+1. **Date**: The date of the activity.
+2. **Start Time**: The time when the activity started.
+3. **Stop Time**: The time when the activity stopped.
+4. **Activity Code**: A unique code representing the activity.
+5. **Activity Description**: A brief description of the activity.
+6. **Delta Time**: The difference in minutes between the start and stop times.
+
+### Example
+```html
+<table BORDER="3">
+  <tbody>
+    <tr>
+        <td> Date </td>
+        <td> Start </td>
+        <td> Stop </td>
+        <td> Activity Code </td>
+        <td> Activity </td>
+        <td> Delta Time </td>
+    </tr>
+    <tr>
+        <td> 08/21/23 </td>
+        <td> 23:10 </td>
+        <td> 01:10 </td>
+        <td> ADMIN </td>
+        <td> Created this README </td>
+        <td> xx </td>
+    </tr>
+    <tr>
+        <td> 08/20/23 </td>
+        <td> 16:10 </td>
+        <td> 19:10 </td>
+        <td> DEV </td>
+        <td> Worked on timelog.py </td>
+        <td> 180 </td>
+    </tr>
+   </tbody>
+</table>
+```
+
+## Functions
+
+1. **`time_delta(row, date_column, start_time_column, end_time_column)`**:
+    - **Description**: Calculates the time difference in minutes between the start and end times for a given row.
+    - **Parameters**: The row data, the indices for the date, start time, and end time columns.
+    - **Returns**: The time difference in minutes.
+
+2. **`create_final_table(table)`**:
+    - **Description**: Converts a list of lists (table) into an HTML table string.
+    - **Parameters**: A list of lists representing the table.
+    - **Returns**: An HTML string representation of the table.
+
+3. **`process_html(soup)`**:
+    - **Description**: Parses an HTML table and returns its rows as lists.
+    - **Parameters**: A BeautifulSoup object containing the HTML table.
+    - **Returns**: A list of rows, where each row is a list of cell contents.
+
+4. **`reorder_table(file_name, date_column, start_time_column, end_time_column)`**:
+    - **Description**: Parses an HTML file containing a table, calculates the time delta for each row, and returns a reordered table.
+    - **Parameters**: The file path of the HTML file, the indices for the date, start time, and end time columns.
+    - **Returns**: An HTML string representing the reordered table.
+
+## Usage
+
+To use the `reorder_table()` function:
+
+```python
+final_table_html = reorder_table('timelogTable.html', 0, 1, 2)
+with open('new.html', 'w') as f:
+    f.write(final_table_html)
+```
+
+This example reads data from `timelogTable.html`, processes it, and writes the reordered table to `new.html`.
+
+## Dependencies
+
+- **BeautifulSoup**: Used for HTML parsing and processing.
+
+## Note
+
+Ensure that the provided HTML file follows the expected table structure mentioned above. Improperly formatted data may lead to unexpected results or errors.
---
+++ timelog.py
@@ -0,0 +1,136 @@
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+
+def time_delta(row, date_column, start_time_column, end_time_column):
+    """
+    Calculate the time delta between start_time and end_time on a given date.
+
+    Parameters:
+    - row (list): A list representing a row of data, containing date and time info.
+    - date_column (int): The index of the date in the list x.
+    - start_time_column (int): The index of the start time in the list x.
+    - end_time_column (int): The index of the end time in the list x.
+
+    Returns:
+    - timedelta: The time difference in minutes between start_time and end_time.
+    """
+    try:
+        date_str = row[date_column]
+        start_time_str = row[start_time_column]
+        end_time_str = row[end_time_column]
+        start_time = datetime.strptime(f"{date_str} {start_time_str}", '%m/%d/%y %H:%M')
+        end_time = datetime.strptime(f"{date_str} {end_time_str}", '%m/%d/%y %H:%M')
+
+        if end_time < start_time:
+            end_time += timedelta(days=1)
+
+      # Calculate the time difference in minutes
+        delta_minutes = (end_time - start_time).total_seconds() / 60
+        return int(delta_minutes)
+
+    except Exception as e:
+        print(f"Error parsing time data: {e}")
+        return None
+
+def create_final_table(table:list) -> str:
+    """
+    Convert a list of lists into an HTML table string.
+
+    Parameters:
+    - table (list): A list of lists where each inner list represents a row of data.
+
+    Returns:
+    - str: An HTML string representation of the table.
+    """
+    # Create an empty table
+    table_html = []
+    table_html.append("<table>\n<tbody>\n")
+    # table_html = BeautifulSoup('<table><tbody></tbody></table>', 'html5lib')
+    # Iterate through the table list and add each row to the table
+    for row in table:
+        row_html = '\n\t'.join([f'<td>{cell}</td>' for cell in row])
+        table_html.append(f"<tr>\n{row_html}\n</tr>\n")
+    table_html.append("\n</tbody>\n</table>\n")
+    # Get the final html
+    return  str('\n'.join(table_html))
+
+def sort_key(row: list, date_column: int, hour_column: int) -> datetime:
+    """
+    Construct a datetime object from the provided data based on specified columns.
+
+    Parameters:
+    - row (list): A list representing a row of data.
+    - date_column (int): Index of the column containing the date.
+    - hour_column (int): Index of the column containing the time.
+
+    Returns:
+    - datetime: A datetime object constructed from the date and time columns.
+    """
+    dt_string = f"{row[date_column]} {row[hour_column]}"
+    return datetime.strptime(dt_string, '%m/%d/%y %H:%M')
+
+def process_html(soup: BeautifulSoup) -> list:
+    """
+    Extract table data from the provided BeautifulSoup object.
+
+    Parameters:
+    - soup (BeautifulSoup): The parsed HTML content using BeautifulSoup.
+
+    Returns:
+    - list: A list of lists containing the extracted table data.
+    """
+    table = []
+    # Process each <tr> element in the parsed HTML
+    all_rows = soup.find_all('tr')
+    for row in all_rows:
+        cells = row.find_all(['th', 'td'])
+        cell_content = [cell.get_text(strip=True) for cell in cells]
+        row.clear()
+        table.append(cell_content)
+    return table
+
+def reorder_table(file_name, date_column, start_time_column, end_time_column):
+    """
+    Parse an HTML file to extract tabular data, and calculate the time delta for each row.
+
+    Parameters:
+    - file_name (str): The path to the HTML file containing the table.
+    - date_column (int): The index of the date column in the table.
+    - start_time_column (int): The index of the start time column in the table.
+    - end_time_column (int): The index of the end time column in the table.
+
+    Returns:
+    - list: A list of rows from the table, each row being a list of cell contents.
+    - list: A list of headers from the table.
+    - list: A list of calculated time deltas for each row.
+    """
+    with open(file_name, 'r') as f:
+        soup = BeautifulSoup(f, 'html5lib')
+    # remove the first row which are the headers
+    table = process_html(soup)
+    headers = table.pop(0)
+
+    new_table = []
+
+    for item in table:
+        time_delta_in_minutes = time_delta(item, 0, 1, 2)
+        new_table.append(item)
+        if item[5] == "xx" or int(item[5]) != int(time_delta_in_minutes):
+            print("{} {} {}".format(item[0], item[5], time_delta_in_minutes))
+            item[5] = int(time_delta_in_minutes)
+    # sort the data according to the selected column and reverse the order
+    sorted_table = sorted(new_table, key=lambda x: sort_key(x, 0, 1), reverse=True)
+
+    # add headers again
+    sorted_table.insert(0, headers)
+    # Reorder the rows of the table
+    # table_html.tbody.clear()
+    # for row in table:
+    #    table_html.tbody.append(BeautifulSoup(f'<tr>{"".join(["<td>{cell}</td>" for cell in row])}</tr>', 'html.parser'))
+    return create_final_table(sorted_table)
+
+
+# Example usage:
+final_table_html = reorder_table('timelogTable.html', 0, 1, 2)
+with open('new.html', 'w') as f:
+    f.write(final_table_html)
